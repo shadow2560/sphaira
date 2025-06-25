shadow2560/sphaira

commit 2ff2923d39d7a87956303001a1986a74ae2a0597

replace almost all uses of shared_ptr with unique_ptr.

sphaira/source/ui/menus/save_menu.cpp
@@ -215,15 +215,12 @@ void FakeNacpEntryForSystem(Entry& e) {
     // fake the nacp entry
     std::snprintf(e.lang.name, sizeof(e.lang.name), "%s | %016lX", GetSystemSaveName(e.system_save_data_id), e.system_save_data_id);
     std::strcpy(e.lang.author, "Nintendo");
-    e.info.reset();
 }
 
-bool LoadControlImage(Entry& e) {
-    if (!e.image && e.info && !e.info->icon.empty()) {
-        ON_SCOPE_EXIT(e.info.reset());
-
+bool LoadControlImage(Entry& e, title::ThreadResultData* result) {
+    if (!e.image && result && !result->icon.empty()) {
         TimeStamp ts;
-        const auto image = ImageLoadFromMemory(e.info->icon, ImageFlag_JPEG);
+        const auto image = ImageLoadFromMemory(result->icon, ImageFlag_JPEG);
         if (!image.data.empty()) {
             e.image = nvgCreateImageRGBA(App::GetVg(), image.w, image.h, 0, image.data.data());
             log_write("\t[image load] time taken: %.2fs %zums\n", ts.GetSecondsD(), ts.GetMs());
@@ -234,8 +231,7 @@ bool LoadControlImage(Entry& e) {
     return false;
 }
 
-void LoadResultIntoEntry(Entry& e, const std::shared_ptr<title::ThreadResultData>& result) {
-    e.info = result;
+void LoadResultIntoEntry(Entry& e, title::ThreadResultData* result) {
     e.status = result->status;
     e.lang = result->lang;
     e.status = result->status;
@@ -251,7 +247,7 @@ void LoadControlEntry(Entry& e, bool force_image_load = false) {
     }
 
     if (force_image_load && e.status == title::NacpLoadStatus::Loaded) {
-        LoadControlImage(e);
+        LoadControlImage(e, title::Get(e.application_id));
     }
 }
 
@@ -330,8 +326,8 @@ Menu::Menu(u32 flags) : grid::Menu{"Saves"_i18n, flags} {
             SetPop();
         }}),
         std::make_pair(Button::X, Action{"Options"_i18n, [this](){
-            auto options = std::make_shared<Sidebar>("Save Options"_i18n, Sidebar::Side::RIGHT);
-            ON_SCOPE_EXIT(App::Push(options));
+            auto options = std::make_unique<Sidebar>("Save Options"_i18n, Sidebar::Side::RIGHT);
+            ON_SCOPE_EXIT(App::Push(std::move(options)));
 
             SidebarEntryArray::Items account_items;
             for (const auto& e : m_accounts) {
@@ -347,9 +343,9 @@ Menu::Menu(u32 flags) : grid::Menu{"Saves"_i18n, flags} {
             data_type_items.emplace_back("Cache"_i18n);
             data_type_items.emplace_back("System BCAT"_i18n);
 
-            options->Add(std::make_shared<SidebarEntryCallback>("Sort By"_i18n, [this](){
-                auto options = std::make_shared<Sidebar>("Sort Options"_i18n, Sidebar::Side::RIGHT);
-                ON_SCOPE_EXIT(App::Push(options));
+            options->Add(std::make_unique<SidebarEntryCallback>("Sort By"_i18n, [this](){
+                auto options = std::make_unique<Sidebar>("Sort Options"_i18n, Sidebar::Side::RIGHT);
+                ON_SCOPE_EXIT(App::Push(std::move(options)));
 
                 SidebarEntryArray::Items sort_items;
                 sort_items.push_back("Updated"_i18n);
@@ -363,36 +359,36 @@ Menu::Menu(u32 flags) : grid::Menu{"Saves"_i18n, flags} {
                 layout_items.push_back("Icon"_i18n);
                 layout_items.push_back("Grid"_i18n);
 
-                options->Add(std::make_shared<SidebarEntryArray>("Sort"_i18n, sort_items, [this](s64& index_out){
+                options->Add(std::make_unique<SidebarEntryArray>("Sort"_i18n, sort_items, [this](s64& index_out){
                     m_sort.Set(index_out);
                     SortAndFindLastFile(false);
                 }, m_sort.Get()));
 
-                options->Add(std::make_shared<SidebarEntryArray>("Order"_i18n, order_items, [this](s64& index_out){
+                options->Add(std::make_unique<SidebarEntryArray>("Order"_i18n, order_items, [this](s64& index_out){
                     m_order.Set(index_out);
                     SortAndFindLastFile(false);
                 }, m_order.Get()));
 
-                options->Add(std::make_shared<SidebarEntryArray>("Layout"_i18n, layout_items, [this](s64& index_out){
+                options->Add(std::make_unique<SidebarEntryArray>("Layout"_i18n, layout_items, [this](s64& index_out){
                     m_layout.Set(index_out);
                     OnLayoutChange();
                 }, m_layout.Get()));
             }));
 
-            options->Add(std::make_shared<SidebarEntryArray>("Account"_i18n, account_items, [this](s64& index_out){
+            options->Add(std::make_unique<SidebarEntryArray>("Account"_i18n, account_items, [this](s64& index_out){
                 m_account_index = index_out;
                 m_dirty = true;
                 App::PopToMenu();
             }, m_account_index));
 
-            options->Add(std::make_shared<SidebarEntryArray>("Data Type"_i18n, data_type_items, [this](s64& index_out){
+            options->Add(std::make_unique<SidebarEntryArray>("Data Type"_i18n, data_type_items, [this](s64& index_out){
                 m_data_type = index_out;
                 m_dirty = true;
                 App::PopToMenu();
             }, m_data_type));
 
             if (m_entries.size()) {
-                options->Add(std::make_shared<SidebarEntryCallback>("Backup"_i18n, [this](){
+                options->Add(std::make_unique<SidebarEntryCallback>("Backup"_i18n, [this](){
                     std::vector<std::reference_wrapper<Entry>> entries;
                     if (m_selected_count) {
                         for (auto& e : m_entries) {
@@ -408,21 +404,21 @@ Menu::Menu(u32 flags) : grid::Menu{"Saves"_i18n, flags} {
                 }, true));
 
                 if (m_entries[m_index].save_data_type == FsSaveDataType_Account || m_entries[m_index].save_data_type == FsSaveDataType_Bcat) {
-                    options->Add(std::make_shared<SidebarEntryCallback>("Restore"_i18n, [this](){
+                    options->Add(std::make_unique<SidebarEntryCallback>("Restore"_i18n, [this](){
                         RestoreSave();
                     }, true));
                 }
             }
 
-            options->Add(std::make_shared<SidebarEntryCallback>("Advanced"_i18n, [this](){
-                auto options = std::make_shared<Sidebar>("Advanced Options"_i18n, Sidebar::Side::RIGHT);
-                ON_SCOPE_EXIT(App::Push(options));
+            options->Add(std::make_unique<SidebarEntryCallback>("Advanced"_i18n, [this](){
+                auto options = std::make_unique<Sidebar>("Advanced Options"_i18n, Sidebar::Side::RIGHT);
+                ON_SCOPE_EXIT(App::Push(std::move(options)));
 
-                options->Add(std::make_shared<SidebarEntryBool>("Auto backup on restore"_i18n, m_auto_backup_on_restore.Get(), [this](bool& v_out){
+                options->Add(std::make_unique<SidebarEntryBool>("Auto backup on restore"_i18n, m_auto_backup_on_restore.Get(), [this](bool& v_out){
                     m_auto_backup_on_restore.Set(v_out);
                 }));
 
-                options->Add(std::make_shared<SidebarEntryBool>("Compress backup"_i18n, m_compress_save_backup.Get(), [this](bool& v_out){
+                options->Add(std::make_unique<SidebarEntryBool>("Compress backup"_i18n, m_compress_save_backup.Get(), [this](bool& v_out){
                     m_compress_save_backup.Set(v_out);
                 }));
             }));
@@ -507,14 +503,12 @@ void Menu::Draw(NVGcontext* vg, Theme* theme) {
                 FakeNacpEntryForSystem(e);
             }
         } else if (e.status == title::NacpLoadStatus::Progress) {
-            if (const auto data = title::GetAsync(e.application_id)) {
-                LoadResultIntoEntry(e, data);
-            }
+            LoadResultIntoEntry(e, title::GetAsync(e.application_id));
         }
 
         // lazy load image
         if (image_load_count < image_load_max) {
-            if (LoadControlImage(e)) {
+            if (LoadControlImage(e, title::GetAsync(e.application_id))) {
                 image_load_count++;
             }
         }
@@ -683,7 +677,7 @@ void Menu::OnLayoutChange() {
 
 void Menu::BackupSaves(std::vector<std::reference_wrapper<Entry>>& entries) {
     dump::DumpGetLocation("Select backup location"_i18n, dump::DumpLocationFlag_SdCard|dump::DumpLocationFlag_Stdio, [this, entries](const dump::DumpLocation& location){
-        App::Push(std::make_shared<ProgressBox>(0, "Backup"_i18n, "", [this, entries, location](auto pbox) -> Result {
+        App::Push(std::make_unique<ProgressBox>(0, "Backup"_i18n, "", [this, entries, location](auto pbox) -> Result {
             for (auto& e : entries) {
                 // the entry may not have loaded yet.
                 LoadControlEntry(e);
@@ -734,7 +728,7 @@ void Menu::RestoreSave() {
         }
 
         if (paths.empty()) {
-            App::Push(std::make_shared<ui::OptionBox>(
+            App::Push(std::make_unique<ui::OptionBox>(
                 "No saves found in "_i18n + fs::AppendPath(fs->Root(), BuildSaveBasePath(m_entries[m_index])).toString(),
                 "OK"_i18n
             ));
@@ -742,7 +736,7 @@ void Menu::RestoreSave() {
         }
 
         const auto title = "Restore save for: "_i18n + m_entries[m_index].GetName();
-        App::Push(std::make_shared<PopupList>(
+        App::Push(std::make_unique<PopupList>(
             title, items, [this, paths, items, location](auto op_index){
                 if (!op_index) {
                     return;
@@ -751,11 +745,11 @@ void Menu::RestoreSave() {
                 const auto file_name = items[*op_index];
                 const auto file_path = paths[*op_index];
 
-                App::Push(std::make_shared<OptionBox>(
+                App::Push(std::make_unique<OptionBox>(
                     "Are you sure you want to restore "_i18n + file_name + "?",
                     "Back"_i18n, "Restore"_i18n, 0, [this, file_path, location](auto op_index){
                         if (op_index && *op_index) {
-                            App::Push(std::make_shared<ProgressBox>(0, "Restore"_i18n, "", [this, file_path, location](auto pbox) -> Result {
+                            App::Push(std::make_unique<ProgressBox>(0, "Restore"_i18n, "", [this, file_path, location](auto pbox) -> Result {
                                 // the entry may not have loaded yet.
                                 LoadControlEntry(m_entries[m_index]);
 
@@ -813,8 +807,8 @@ Result Menu::RestoreSaveInternal(ProgressBox* pbox, const Entry& e, const fs::Fs
     pbox->SetTitle(e.GetName());
     if (e.image) {
         pbox->SetImage(e.image);
-    } else if (e.info && !e.info->icon.empty()) {
-        pbox->SetImageDataConst(e.info->icon);
+    } else if (auto data = title::Get(e.application_id); data && !data->icon.empty()) {
+        pbox->SetImageDataConst(data->icon);
     } else {
         pbox->SetImage(0);
     }
@@ -940,8 +934,8 @@ Result Menu::BackupSaveInternal(ProgressBox* pbox, const dump::DumpLocation& loc
     pbox->SetTitle(e.GetName());
     if (e.image) {
         pbox->SetImage(e.image);
-    } else if (e.info && !e.info->icon.empty()) {
-        pbox->SetImageDataConst(e.info->icon);
+    } else if (auto data = title::Get(e.application_id); data && !data->icon.empty()) {
+        pbox->SetImageDataConst(data->icon);
     } else {
         pbox->SetImage(0);
     }

sphaira/CMakeLists.txt
@@ -150,6 +150,7 @@ target_compile_options(sphaira PRIVATE
     -Wimplicit-fallthrough=5
     -Wsuggest-final-types
     -Wuninitialized
+    -fdiagnostics-all-candidates
 )
 
 include(FetchContent)

sphaira/include/app.hpp
@@ -58,7 +58,7 @@ public:
     static void Exit();
     static void ExitRestart();
     static auto GetVg() -> NVGcontext*;
-    static void Push(std::shared_ptr<ui::Widget>);
+    static void Push(std::unique_ptr<ui::Widget>&&);
     // pops all widgets above a menu
     static void PopToMenu();
 
@@ -260,7 +260,7 @@ public:
 
     Vec2 m_scale{1, 1};
 
-    std::vector<std::shared_ptr<ui::Widget>> m_widgets;
+    std::vector<std::unique_ptr<ui::Widget>> m_widgets;
     u32 m_pop_count{};
     ui::NotifMananger m_notif_manager{};
 

sphaira/include/dumper.hpp
@@ -55,10 +55,10 @@ using OnExit = std::function<void(Result rc)>;
 using OnLocation = std::function<void(const DumpLocation& loc)>;
 
 // prompts the user to select dump location, calls on_loc on success with the selected location.
-void DumpGetLocation(const std::string& title, u32 location_flags, OnLocation on_loc);
+void DumpGetLocation(const std::string& title, u32 location_flags, const OnLocation& on_loc);
 // dumps to a fetched location using DumpGetLocation().
-void Dump(std::shared_ptr<BaseSource> source, const DumpLocation& location, const std::vector<fs::FsPath>& paths, OnExit on_exit);
+void Dump(const std::shared_ptr<BaseSource>& source, const DumpLocation& location, const std::vector<fs::FsPath>& paths, const OnExit& on_exit);
 // DumpGetLocation() + Dump() all in one.
-void Dump(std::shared_ptr<BaseSource> source, const std::vector<fs::FsPath>& paths, OnExit on_exit = [](Result){}, u32 location_flags = DumpLocationFlag_All);
+void Dump(const std::shared_ptr<BaseSource>& source, const std::vector<fs::FsPath>& paths, const OnExit& on_exit = [](Result){}, u32 location_flags = DumpLocationFlag_All);
 
 } // namespace sphaira::dump

sphaira/include/hasher.hpp
@@ -25,7 +25,7 @@ struct BaseSource {
 auto GetTypeStr(Type type) -> const char*;
 
 // returns the hash string.
-Result Hash(ui::ProgressBox* pbox, Type type, std::shared_ptr<BaseSource> source, std::string& out);
+Result Hash(ui::ProgressBox* pbox, Type type, BaseSource* source, std::string& out);
 Result Hash(ui::ProgressBox* pbox, Type type, fs::Fs* fs, const fs::FsPath& path, std::string& out);
 Result Hash(ui::ProgressBox* pbox, Type type, std::span<const u8> data, std::string& out);
 

sphaira/include/title_info.hpp
@@ -60,9 +60,9 @@ void Clear();
 // adds new entry to queue.
 void PushAsync(u64 app_id);
 // gets entry without removing it from the queue.
-auto GetAsync(u64 app_id) -> std::shared_ptr<ThreadResultData>;
+auto GetAsync(u64 app_id) -> ThreadResultData*;
 // single threaded title info fetch.
-auto Get(u64 app_id, bool* cached = nullptr) -> std::shared_ptr<ThreadResultData>;
+auto Get(u64 app_id, bool* cached = nullptr) -> ThreadResultData*;
 
 auto GetNcmCs(u8 storage_id) -> NcmContentStorage&;
 auto GetNcmDb(u8 storage_id) -> NcmContentMetaDatabase&;

sphaira/include/ui/menus/appstore.hpp
@@ -105,9 +105,9 @@ private:
     LazyImage m_banner{};
     std::unique_ptr<List> m_list{};
 
-    std::shared_ptr<ScrollableText> m_details{};
-    std::shared_ptr<ScrollableText> m_changelog{};
-    std::shared_ptr<ScrollableText> m_detail_changelog{};
+    std::unique_ptr<ScrollableText> m_details{};
+    std::unique_ptr<ScrollableText> m_changelog{};
+    ScrollableText* m_detail_changelog{};
     std::unique_ptr<ScrollableText> m_manifest_list{};
 
     bool m_show_changlog{};

sphaira/include/ui/menus/filebrowser.hpp
@@ -300,7 +300,7 @@ private:
 
 // contains all selected files for a command, such as copy, delete, cut etc.
 struct SelectedStash {
-    void Add(std::shared_ptr<FsView> view, SelectedType type, const std::vector<FileEntry>& files, const fs::FsPath& path) {
+    void Add(FsView* view, SelectedType type, const std::vector<FileEntry>& files, const fs::FsPath& path) {
         if (files.empty()) {
             Reset();
         } else {
@@ -335,7 +335,7 @@ struct SelectedStash {
     }
 
 // private:
-    std::shared_ptr<FsView> m_view{};
+    FsView* m_view{};
     std::vector<FileEntry> m_files{};
     fs::FsPath m_path{};
     SelectedType m_type{SelectedType::None};
@@ -389,9 +389,9 @@ private:
 private:
     static constexpr inline const char* INI_SECTION = "filebrowser";
 
-    std::shared_ptr<FsView> view{};
-    std::shared_ptr<FsView> view_left{};
-    std::shared_ptr<FsView> view_right{};
+    FsView* view{};
+    std::unique_ptr<FsView> view_left{};
+    std::unique_ptr<FsView> view_right{};
 
     std::vector<FileAssocEntry> m_assoc_entries{};
     SelectedStash m_selected{};

commit 299aaa53593ca8be6712a60fcc0de34d3b0983d5

fix save menu crash caused by commit 2ff2923.

sphaira/source/ui/menus/save_menu.cpp
@@ -232,9 +232,11 @@ bool LoadControlImage(Entry& e, title::ThreadResultData* result) {
 }
 
 void LoadResultIntoEntry(Entry& e, title::ThreadResultData* result) {
-    e.status = result->status;
-    e.lang = result->lang;
-    e.status = result->status;
+    if (result) {
+        e.status = result->status;
+        e.lang = result->lang;
+        e.status = result->status;
+    }
 }
 
 void LoadControlEntry(Entry& e, bool force_image_load = false) {